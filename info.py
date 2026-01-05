c35f6db8eff4240d1c9c60f39df2be1e68bbaa7c
arushk16862-beep <arushk16862@gmail.com>
2026-01-05 08:13:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,9 +12,9 @@ id_pattern = re.compile(r'^.\d+$')
 # 🔹 BOT INFORMATION
 # ============================================================
 SESSION = environ.get('SESSION', 'MyselfNeon')
-API_ID = int(environ.get('API_ID', '20447284'))
-API_HASH = environ.get('API_HASH', 'b215f8e05089c399ed89170544621a70')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7996680610:AAG8leAamsgJilKuninAhe8WSRqLF62p2UM")
+API_ID = int(environ.get('API_ID', '21383441'))
+API_HASH = environ.get('API_HASH', '62141d4cefa2756e4059cbdeac6ab46b')
+BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Keep-Alive URL
 KEEP_ALIVE_URL = environ.get(
@@ -276,6 +276,7 @@ REACTIONS = [
 # Dont remove Credits
 # Developer Telegram @MyselfNeon
 # Update channel - @NeonFiles
+
 
 
 
